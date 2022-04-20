b8f834b71d1e9a3df216640e594628b478c45a07
여정동 <duwjdehd0713@gmail.com>
2022-04-20 10:44:17 +0900

    Feat Ipad air 컴포넌트 프로토타입

--- apple-clone/src/App.js
+++ apple-clone/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import IpadAir from './components_jd/ipad_air';
 
 export default function App() {
   return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div>
+      <IpadAir></IpadAir>
+    </div>
   )
 }
---
+++ apple-clone/src/components_jd/ipad_air.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class IpadAir extends React.Component {
+  render() {
+    return (
+      <section id="ipad_section" class="grid grid-cols-5 mx-auto bg-black text-white p-10 flex items-center">
+        <div id="section-content" class="text-center col-span-2 col-start">
+          <div class="font-bold p-3">
+            <p class="text-xs text-orange-500 font-normal">New</p>
+            <span class="text-6xl">iPad </span>
+            <span class="text-6xl text-purple-300">air</span>
+            <p class="text-2xl">가뿐하게. 색다르게.</p>
+            <p class="text-2xl">강력하게.</p>
+          </div>
+          <p class="p-2 text-lg">₩779,000부터</p>
+          <div class="p-1">
+            <button class="bg-blue-600 hover:bg-blue-500 text-white py-2 px-4 rounded-full">
+              구입하기
+            </button>
+          </div>
+          <div class="p-2">
+            <a href="#" class="text-blue-500">
+              더 알아보기 >
+             </a>
+          </div>
+        </div>
+        <div id="section-image" class="col-span-3">
+          <img src="asset/image/ipad_air_main.png"></img>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default IpadAir;
