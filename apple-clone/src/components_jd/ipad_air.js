d1ed529196b6828acda92edd99ed24b6f663bcd8
여정동 <duwjdehd0713@gmail.com>
2022-04-21 10:44:37 +0900

    Feat iPad air 컴포넌트 반응형 UI 추가

--- apple-clone/src/components_jd/ipad_air.js
+++ apple-clone/src/components_jd/ipad_air.js
@@ -1,38 +1,96 @@
 import React from "react";
-import { useMediaQuery } from "react-responsive"
+import { useMediaQuery } from "react-responsive";
 
-class IpadAir extends React.Component {
+function IpadAir() {
+  const isPC = useMediaQuery({
+    query: "(min-width:1024px)",
+  });
+  const isTablet = useMediaQuery({
+    query: "(min-width:768px) and (max-width:1023px)",
+  });
+  const isMobile = useMediaQuery({
+    query: "(max-width:767px)",
+  });
 
-  render() {
-
-    return (
-      <div id="ipad_section" class="grid grid-cols-5 mx-auto bg-black text-white px-20 py-10 flex items-center">
-        <div id="section-content" class="text-center col-span-2 col-start">
-          <div class="p-3 m-1">
-            <p class="text-xs text-orange-500 m-3">New</p>
-            <span class="text-6xl font-semibold">iPad </span>
-            <span class="text-6xl text-purple-300">air</span>
-            <p class="text-xl font-bold mt-1">가뿐하게. 색다르게.</p>
-            <p class="text-xl font-bold">강력하게.</p>
+  return (
+    <div>
+      {(isPC || isTablet) && (
+        <div
+          id="ipad_section"
+          class="grid grid-cols-5 mx-auto bg-black text-white px-20 py-10 flex items-center"
+        >
+          <div id="section-content" class="text-center col-span-2 col-start">
+            {isPC && (
+              <div class="p-3 m-1">
+                <p class="text-xs text-orange-500 m-3">New</p>
+                <span class="text-6xl font-semibold">iPad </span>
+                <span class="text-6xl text-purple-300">air</span>
+                <p class="text-xl font-bold mt-1">가뿐하게. 색다르게.</p>
+                <p class="text-xl font-bold">강력하게.</p>
+                <p class="p-2 text-lg" alt="">
+                  ₩779,000부터
+                </p>
+              </div>
+            )}
+            {isTablet && (
+              <div class="p-2 m-1">
+                <p class="text-xs text-orange-500 m-3">New</p>
+                <span class="text-5xl font-semibold">iPad </span>
+                <span class="text-5xl text-purple-300">air</span>
+                <p class="text-lg font-bold mt-1">가뿐하게. 색다르게.</p>
+                <p class="text-lg font-bold">강력하게.</p>
+                <p class="p-2" alt="">
+                  ₩779,000부터
+                </p>
+              </div>
+            )}
+            <div class="p-1">
+              <button class="bg-blue-600 hover:bg-blue-500 text-white py-2 px-4 rounded-full">
+                구입하기
+              </button>
+            </div>
+            <div class="p-2">
+              <a href="#" class="text-blue-400">
+                더 알아보기 >
+              </a>
+            </div>
           </div>
-          <p class="p-2 text-lg">₩779,000부터</p>
-          <div class="p-1">
-            <button class="bg-blue-600 hover:bg-blue-500 text-white py-2 px-4 rounded-full">
-              구입하기
-            </button>
-          </div>
-          <div class="p-2">
-            <a href="#" class="text-blue-400">
-              더 알아보기 >
-             </a>
-          </div>
-        </div>
-        <div id="section-image" class="col-span-3">
+          <div id="section-image" class="col-span-3">
             <img src="asset/image/ipad_air_main.png"></img>
+          </div>
         </div>
-      </div>
-    );
-  }
+      )}
+      {isMobile && 
+        <div
+        id="ipad_section"
+        class="mx-auto bg-black text-white px-20 py-10 items-center">
+          <div class="p-2 m-1 text-center">
+                <p class="text-xs text-orange-500 m-3">New</p>
+                <span class="text-5xl font-semibold">iPad </span>
+                <span class="text-5xl text-purple-300">air</span>
+                <p class="text-lg font-bold mt-1">가뿐하게. 색다르게.</p>
+                <p class="text-lg font-bold">강력하게.</p>
+                <p class="p-2" alt="">
+                  ₩779,000부터
+                </p>
+                <div class="p-1">
+              <button class="bg-blue-600 hover:bg-blue-500 text-white py-2 px-4 rounded-full">
+                구입하기
+              </button>
+            </div>
+            <div class="p-2">
+              <a href="#" class="text-blue-400">
+                더 알아보기 >
+              </a>
+            </div>
+          </div>
+          <div id="section-image">
+            <img src="asset/image/ipad_air_main.png"></img>
+          </div>
+        </div>
+      }
+    </div>
+  );
 }
 
 export default IpadAir;
---
+++ apple-clone/src/components_jd/ipad_component.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+function ipad_components(){
+
+}
+
+//export default ipad_components;
