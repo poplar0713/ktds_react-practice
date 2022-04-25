f2740db996136bb97a65705d82d8fab009f3779a
박서연 <psy3153@naver.com>
2022-04-25 21:10:56 +0900

    Init : React initial commit

---
+++ apple-clone/src/components_airpod/AirpodComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Airpod.css';
+import AirpodMenu from './AirpodMenu';
+
+function AirpodComponent() {
+  return (
+    <div>
+      <AirpodMenu />
+    </div>
+  );
+}
+
+export default AirpodComponent;
---
+++ apple-clone/src/components_airpod/AirpodMenu.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function AirpodMenu() {
+  return (
+    <nav>
+      <div class="chapternav-wrapper">
+        <ul class="chapternav-items">
+          <li class="chapternav-item chapternav-item-airpods-2gen">
+            <a class="chapternav-link">
+              <figure class="chapternav-icon"></figure>
+              <span>
+                "AirPods"
+                <br />
+                "2세대"
+              </span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default AirpodMenu;
